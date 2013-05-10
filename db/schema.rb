a565b880e93a4734b6361c60b337e52d3cc8c1aa
Karan Hiremath <karanhir@seas.upenn.edu>
2013-05-10 11:16:11 -0400

    Adding user model with authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130508193336) do
+ActiveRecord::Schema.define(:version => 20130510150120) do
 
   create_table "articles", :force => true do |t|
     t.string   "content"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(:version => 20130508193336) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
