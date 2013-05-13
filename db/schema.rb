66ad082e0dc9180f5cb2b9d1588c711898aa351a
Spencer Bard <spencerbard@Spencers-MacBook-Pro.local>
2013-05-13 11:35:51 -0400

    please work

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130510150120) do
 
-  create_table "articles", :force => true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
